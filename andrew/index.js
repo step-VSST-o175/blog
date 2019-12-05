7a0c49263dbd63b72d7f2da5baa7ded748e598f3
bitbybitstereokid <bitbybitstreokid@gmail.com>
2019-12-05 20:32:36 +0200

    Add folder andrew

--- andrew/index.js
+++ andrew/index.js
@@ -1,26 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./src/config');
+const db = require('./src/db/index');
 
 const app = express();
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send('Hello World! ewfrewfw');
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  try {
+    const rows = await db.query('SELECT * from posts');
+    console.log(rows);
+    res.render('pages/index'){
+      posts,
+    }
+  }
+  catch (error) {
+    res.render('pages/erroe');
+  }
 });
 
-app.post('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send('Hello World! ewfrewfw');
-});
+// app.get('/', (req, res) => {
+//   console.log(req.query);
+//   console.log(req.body);
+//   // res.send('Hello World! 1');
+//   res.render('pages/index');
+// });
+//
+// app.post('/', (req, res) => {
+//   console.log(req.query);
+//   console.log(req.body);
+//   res.send('Hello World! 2');
+// });
+//
+// app.patch('/', (req, res) => {
+//   console.log(req.query);
+//   console.log(req.body);
+//   res.send('Hello World! 3');
+// });
+//
+// app.put('/', (req, res) => {
+//   console.log(req.query);
+//   console.log(req.body);
+//   res.send('Hello World! 4');
+// });
 
 app.listen(config.server.port, () => {
   console.log(`App listening on port ${config.server.port}!`);
