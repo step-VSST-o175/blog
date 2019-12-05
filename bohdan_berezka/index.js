e66b0d7eaf964251e45fc699f12c1209d9ee57d7
In-finitum <bohdan.berezka1@gmail.com>
2019-12-05 19:12:09 +0200

    + Postman

--- bohdan_berezka/index.js
+++ bohdan_berezka/index.js
@@ -1,7 +1,27 @@
-const db = require('./src/db');
+const express = require('express');
+const bodyParser = require('body-parser');
+const config = require('./src/config');
 
-db.query('SELECT * from posts')
-  .then((rows) => {
-    console.log(rows);
-    return db.close();
-  });
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.send('Hello World! ewfrewfw');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  res.send('Hello World! ewfrewfw');
+});
+
+app.listen(config.server.port, () => {
+  console.log(`App listening on port ${config.server.port}!`);
+});
