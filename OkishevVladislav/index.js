d4533007d10199c7830387a685c267408d83521f
VladislavOkishev <kronchikx@gmail.com>
2019-12-05 20:53:18 +0200

    Add folder OkishevVladislav 05/12/2019

--- OkishevVladislav/index.js
+++ OkishevVladislav/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./src/config');
+const db = require('./src/db/index');
 
 const app = express();
 
@@ -9,32 +10,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.render('pages/index');
+app.get('/', async (req, res) => {
+  try {
+    const posts = await db.query('SELECT * from posts');
+
+    console.log(posts);
+
+    res.render('pages/index/index', {
+      posts,
+    });
+  } catch (error) {
+    res.render('pages/error');
+  }
 });
 
-app.post('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send('Hello World! post');
-});
-
-app.patch('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send('Hello World! patch');
-});
-
-app.put('/', (req, res) => {
-  console.log(req.query);
-  console.log(req.body);
-  res.send('Hello World! put');
-});
 
 app.listen(config.server.port, () => {
   console.log(`App listening on port ${config.server.port}!`);
