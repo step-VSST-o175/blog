d4ba83105c3cb9c6ccb5f066600cf75e87ec53e4
step <relogn@gmail.com>
2019-12-05 18:50:20 +0200

    Add server

--- slavik-m/index.js
+++ slavik-m/index.js
@@ -1,7 +1,12 @@
-const db = require('./src/db');
+const express = require('express');
+const config = require('./src/config');
 
-db.query('SELECT * from posts')
-  .then((rows) => {
-    console.log(rows);
-    // return db.close();
-  });
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(config.server.port, () => {
+  console.log(`App listening on port ${config.server.port}!`);
+});
